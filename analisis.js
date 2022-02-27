cb5ebc58baced7d173d478da4f354a89e89055e3
DMFotoYDiseno <davidmunozsaez@gmail.com.com>
2022-02-27 18:13:01 -0500

    Añadido index.html con lista de links a todos los proyectos

---
+++ analisis.js
@@ -0,0 +1,58 @@
+//Helpers
+function esPar(numero) {
+    return numero % 2  === 0;
+}
+
+const salariosCol = colombia.map(
+    function(persona) {
+        return persona.salary;
+    }
+);
+
+const salariosColSorted = salariosCol.sort(
+    function(salaryA, salaryB) {
+        return salaryA-salaryB;
+    }
+);
+
+function medianaSalarios(lista) {
+    const mitad = parseInt(lista.length / 2);
+
+    if (esPar(lista.length)) {
+        const personaMitad1 = lista[mitad - 1];
+        const personaMitad2 = lista[mitad]
+
+        const mediana = calcularMediaAritmetica([personaMitad1,personaMitad2])
+        return mediana;
+    } else {
+        const personaMitad = lista[mitad];
+        return personaMitad;
+    }
+};
+
+function calcularMediaAritmetica(lista) {
+       const sumaLista = lista.reduce(
+           function (valorAcumulado = 0, NuevoElemento) {
+               return valorAcumulado + NuevoElemento;
+           }
+       );
+       const promedioLista = sumaLista / lista.length;
+   
+       return promedioLista;
+   };
+
+const medianaGeneralCol = medianaSalarios(salariosCol);
+
+//Mediana del top 10%
+
+const spliceStart = parseInt((salariosColSorted.length * 90)/100);
+const spliceCount = salariosColSorted.length - spliceStart;
+const salariosColTop10 = salariosColSorted.splice(spliceStart,spliceCount);
+
+const medianaTop10Col = medianaSalarios(salariosColTop10);
+
+
+   console.log({
+    medianaGeneralCol,
+    medianaTop10Col,
+   });
